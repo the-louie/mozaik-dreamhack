432679445f56759a4c2374465258dbdcb2d4a1e2
Anders Green <louie@louie.se>
2018-06-17 22:47:13 +0200

    fixes for histogram

--- src/register_apis.js
+++ src/register_apis.js
@@ -4,5 +4,5 @@ module.exports = (Mozaik, configFile, config) => {
   Mozaik.registerApi('github', require('@mozaik/ext-github/client'))
   Mozaik.registerApi('travis', require('@mozaik/ext-travis/client'))
   Mozaik.registerApi('gitlab', require('@mozaik/ext-gitlab/client'))
-  Mozaik.registerApi('jsongraph', require('@mozaik/ext-jsongraph/client'))
+  Mozaik.registerApi('jsongraph', require('mozaik-ext-jsongraph/client'))
 }
--- src/register_extensions.js
+++ src/register_extensions.js
@@ -1,10 +1,10 @@
 import { Registry } from '@mozaik/ui'
 
 import github from '@mozaik/ext-github'
-import jsongraph from '@mozaik/ext-jsongraph'
 import gitlab from '@mozaik/ext-gitlab'
 import time from '@mozaik/ext-time'
 import travis from '@mozaik/ext-travis'
+import jsongraph from 'mozaik-ext-jsongraph'
 
 Registry.addExtensions({
   github,
